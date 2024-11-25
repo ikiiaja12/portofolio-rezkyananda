b09478d4ebb52fce9116ef01003730e683630bab
ikiiaja12 <buanaraksya@gmail.com>
2024-11-25 11:26:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Menambahkan interaksi di halaman (contoh: Scroll Animations, Form Validation)
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Pesan Anda telah terkirim!');
+  });
+  
